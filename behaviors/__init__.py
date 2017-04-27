89526ac7981f974a622259c004dac52cf33e446a
Joseph Vessella <jvessella@cert.org>
2017-04-27 14:18:45 -0400

    Added the dynamic loader for modules.

---
+++ behaviors/__init__.py
@@ -0,0 +1,24 @@
+import os
+import importlib
+
+__all__ = list()
+loaded = list()
+
+py_files = os.listdir(os.path.dirname(__file__))
+
+for candidate in py_files:
+    if candidate == '__init__.py' or candidate[-3:] != '.py':
+        continue
+    module = 'modules.' + candidate[:-3]
+    try:
+        loaded_module = importlib.import_module(module)
+    except ImportError:
+        print('Could not load module %s' % module)
+        continue
+    else:
+        print('Loaded module %s' % module)
+        __all__.append(candidate[:-3])
+        loaded.append(loaded_module)
+del module
+del candidate
+del py_files
