2ba5dccec5d2603e9c954a8367000052514ca7cd
Joseph Vessella <jvessella@cert.org>
2017-05-24 15:54:57 -0400

    Implemented new_task function.

--- api.py
+++ api.py
@@ -1,8 +1,23 @@
-import usersimcore
+import usersim
+from usersim import States
+import tasks
 
 
-def new_task(task_config):
-    pass
+def new_task(task_config, start_paused=False):
+    """ Inserts a new task into the user simulator.
+
+    Arguments:
+        task_config (dict): A dictionary with the following key:value pairs.
+            'type':str
+            'config':dict
+        start_paused (bool): True if the new task should be paused initially, False otherwise.
+
+    Returns:
+        int: The new task's unique ID.
+    """
+    us = usersim.UserSim()
+    new_task = tasks.task_dict[task_config['type']].config(task_config['config'])
+    return us.new_task(new_task, start_paused)
 
 def pause_task(task_id):
     pass
