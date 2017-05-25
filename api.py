12aa6da8812aeb8abf2e378034664e4bcb7f940d
Joseph Vessella <jvessella@cert.org>
2017-05-25 13:11:17 -0400

    [GUS-2426] Current API functions filled in.

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+""" Module which contains all public user simulator operations. All functions contained in this module are thread-safe
+unless otherwise noted.
+"""
 import usersim
 from usersim import States
 import tasks
@@ -15,30 +18,89 @@ def new_task(task_config, start_paused=False):
     Returns:
         int: The new task's unique ID.
     """
-    us = usersim.UserSim()
+    sim = usersim.UserSim()
     new_task = tasks.task_dict[task_config['type']].config(task_config['config'])
-    return us.new_task(new_task, start_paused)
+    return sim.new_task(new_task, start_paused)
 
 def pause_task(task_id):
-    pass
+    """ Pause a single task.
+
+    Arguments:
+        task_id (int): The task ID returned by an earlier call to new_task.
+
+    Returns:
+        bool: True if the operation succeeded, False otherwise.
+    """
+    sim = usersim.UserSim()
+    return sim.pause_task(task_id)
 
 def pause_all():
-    pass
+    """ Pause all currently scheduled tasks.
+    """
+    sim = usersim.UserSim()
+    sim.pause_all()
 
 def unpause_task(task_id):
-    pass
+    """ Unpause a single task.
+
+    Arguments:
+        task_id (int): The task ID returned by an earlier call to new_task.
+
+    Returns:
+        bool: True if the operation succeeded, False otherwise.
+    """
+    sim = usersim.UserSim()
+    return sim.unpause_task(task_id)
 
 def unpause_all():
-    pass
+    """ Unpause all currently scheduled tasks.
+    """
+    sim = usersim.UserSim()
+    sim.unpause_all()
 
 def status_task(task_id):
-    pass
+    """ Get the status of a single task.
+
+    Arguments:
+        task_id (int): The task ID returned by an earlier call to new_task.
+
+    Returns:
+        dict: A dictionary with the following key:value pairs:
+            'id':int
+            'type':str
+            'state':str
+            'status':str
+    """
+    sim = usersim.UserSim()
+    return sim.status_task(task_id)
 
 def status_all():
-    pass
+    """ Get a list of the status of all managed tasks.
+
+    Returns:
+        list of dicts: Each dictionary will have the following key:value pairs:
+            'id':int
+            'type':str
+            'state':str
+            'status':str
+    """
+    sim = usersim.UserSim()
+    return sim.status_all()
 
 def stop_task(task_id):
-    pass
+    """ Stop a single task.
+
+    Arguments:
+        task_id (int): The task ID returned by an earlier call to new_task.
+
+    Returns:
+        bool: True if the operation succeeded, False otherwise.
+    """
+    sim = usersim.UserSim()
+    return sim.stop_task(task_id)
 
 def stop_all():
-    pass
+    """ Stop all tasks that are currently scheduled or paused.
+    """
+    sim = usersim.UserSim()
+    sim.stop_all()
