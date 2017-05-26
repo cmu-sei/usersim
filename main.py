5f91dc03bcd910f0bb81fb77e8974dcf090c0d25
Joseph Vessella <jvessella@cert.org>
2017-05-26 15:45:44 -0400

    Updated main to work with newer components.

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
 import traceback
 
+import api
 import tasks
+import usersim
 
 
 def main():
-
     conf_dict = {'type': 'frequency',
                  'config': {'frequency': 720,
                             'repetitions': 10,
-                            'behavior': {'type': 'printstuff',
-                                         'config': {}}}}
+                            'task': {'type': 'test',
+                                     'config': {}}}}
 
-    behavior_set = set()
+    sim = usersim.UserSim()
+    api.new_task(conf_dict)
 
-    new_behavior = tasks.task_dict[conf_dict['type']].config(conf_dict['config'])
-    behavior_set.add(new_behavior)
-
-    while behavior_set:
-        new_behaviors = set()
-        delete_behaviors = set()
-
-        for task in behavior_set:
-            try:
-                result = task()
-            except Exception:
-                print(traceback.format_exc())
-            else:
-                if result is None:
-                    if task.stop():
-                        task.cleanup()
-                        delete_behaviors.add(task)
-                else:
-                    try:
-                        behavior_obj = tasks.task_dict[result['type']].config(result['config'])
-                    except KeyError as e:
-                        print(e)
-                    except ValueError as e:
-                        print(e)
-                    else:
-                        new_behaviors.add(behavior_obj)
-
-        behavior_set |= new_behaviors
-        behavior_set -= delete_behaviors
+    while True:
+        result = sim.cycle()
+        if len(result) > 1:
+            print(result)
 
 if __name__ == '__main__':
     main()
