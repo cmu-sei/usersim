f8c356705b70c9b1bace0b650583b09b629e88f6
Joseph Vessella <jvessella@cert.org>
2017-06-07 12:42:02 -0400

    [GUS-2428] Added more validation on input.

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ def yaml_to_python(yaml_string):
     Returns:
         list of dicts: The YAML document converted to Python objects, which will be a list of dicts.
             YAML constructs will be converted to Python objects by this conversion:
-                None from -, null
+                None from -, null (except when - is used for list notation)
                 bool from true, false, on, and off
                 int from 42
                 float from 3.14159
@@ -43,6 +43,17 @@ def yaml_to_python(yaml_string):
 
 def string_to_python(config_string):
     """ Converts a string in a supported language format to a list of dicts.
+
+    Arguments:
+        config_string (str): A string configuration in one of the supported formats (YAML, JSON, XML).
+            This string should represent a list of dictionaries, each with the keys 'type' and 'config'.
+
+    Returns:
+        list of dicts: A Python list which includes any number of task configuration dictionaries, each with the keys
+            'type' and 'config'.
+
+    Raises:
+        ValueError: If the given config_string does not meet its requirements.
     """
     converters = [json_to_python,
                   xml_to_python,
@@ -50,8 +61,18 @@ def string_to_python(config_string):
 
     for converter in converters:
         try:
-            return converter(config_string)
+            structure = converter(config_string)
         except Exception:
             continue
+        else:
+            if not isinstance(structure, list):
+                raise ValueError('Invalid configuration - a list could not be inferred.')
+            for task in structure:
+                if not isinstance(task, dict):
+                    raise ValueError('Invalid configuration - a list item is empty.')
+                if 'type' not in task:
+                    raise ValueError('Invalid configuration - a task is missing a type.')
+                if 'config' not in task:
+                    raise ValueError('Invalid configuration - a task is missing its configuration.')
 
     raise ValueError('Could not determine a valid structure from the given configuration string:\n' + config_string)
