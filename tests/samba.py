7a9d3c0b0eaae05c37b86bb7b79d52859d6b79d1
akidwai <mkidwai@sei.cmu.edu>
2017-07-17 15:59:56 -0400

    Changed style to match main program.

--- tests/samba.py
+++ tests/samba.py
@@ -4,18 +4,18 @@
 import api
 import usersim
 
-def testBadKeyCases(task, badKeyCases):
+def test_bad_key_cases(task, bad_key_cases):
     """ Used to test configs with missing keys. This function will raise an assertion error if validate incorrectly 
     accepts a bad config dictionary.
 
     Args:
         task: A task dictionary mapping "type" to the task name (e.g. "ssh")
-        badKeyCases: A list of tuples of the form ("configName", config). config should be missing at least one key.
+        bad_key_cases: A list of tuples of the form ("configName", config). config should be missing at least one key.
 
     Raises:
         AssertionError: If api.new_task does not raise a KeyError
     """
-    for configName, config in badKeyCases:
+    for configName, config in bad_key_cases:
         task['config'] = config
         try:
             api.new_task(task)
@@ -23,19 +23,19 @@ def testBadKeyCases(task, badKeyCases):
         except KeyError:
             print("Correctly rejected %s" % configName)
 
-def testBadValueCases(task, badValueCases):
+def test_bad_value_cases(task, bad_value_cases):
     """ Used to test configs with invalid values. This function will raise an assertion error if validate incorrectly 
     accepts a bad config dictionary.
 
     Args:
         task: A task dictionary mapping "type" to the task name (e.g. "ssh")
-        badValueCases: A list of tuples of the form ("configName", config). config should have at least one invalid 
+        bad_value_cases: A list of tuples of the form ("configName", config). config should have at least one invalid 
             value.
 
     Raises:
         AssertionError: If api.new_task does not raise a ValueError
     """
-    for configName, config in badValueCases:
+    for configName, config in bad_value_cases:
         task['config'] = config
         try:
             api.new_task(task)
@@ -43,15 +43,15 @@ def testBadValueCases(task, badValueCases):
         except ValueError:
             print("Correctly rejected %s" % configName)
 
-def testGoodCases(task, goodCases):
+def test_good_cases(task, good_cases):
     """ Used to test properly formatted configs. Prints feedback from the task.
 
     Args:
         task: A task dictionary mapping "type" to the task name (e.g. "ssh")
-        goodCases: A list of tuples of the form ("configName", config). config should be properly formatted.
+        good_cases: A list of tuples of the form ("configName", config). config should be properly formatted.
     """
     sim = usersim.UserSim(True)
-    for configName, config in goodCases:
+    for configName, config in good_cases:
         task['config'] = config
         api.new_task(task)
         print('Correctly accepted %s' % configName)
@@ -61,24 +61,25 @@ def testGoodCases(task, goodCases):
             print('    Feedback from task:')
             print('    %s' % str(result))
 
-def testSamba():
+def test_samba():
     task = {'type': 'samba', 'config': None}
     empty = {}
-    noneAddress = {'address': None}
-    emptyAddress = {'address': ''}
-    nonePort = {'address': 'localhost', 'port': None}
-    badPort = {'address': 'localhost', 'port': 1000000000}
-    badFiles = {'address': 'localhost', 'files': [None, 1, "README"]}
+    none_address = {'address': None}
+    empty_address = {'address': ''}
+    none_port = {'address': 'localhost', 'port': None}
+    bad_port = {'address': 'localhost', 'port': 1000000000}
+    bad_files = {'address': 'localhost', 'files': [None, 1, "README"]}
     random1 = {'address': 'localhost'}
     random2 = {'address': 'localhost', 'files': []}
     authenticated = {'address': 'localhost', 'user': '', 'passwd': ''}
     complete = {'address': 'localhost', 'user': '', 'passwd': '', 'files': []}
-    badKeyCases = [('empty', empty)]
-    badValueCases = [('noneAddress', noneAddress), ('nonePort', nonePort), ('badPort', badPort), ('badFiles', badFiles)]
-    goodCases = [('random1', random1), ('random2', random2), ('authenticated', authenticated), ('complete', complete)]
-    testBadKeyCases(task, badKeyCases)
-    testBadValueCases(task, badValueCases)
-    testGoodCases(task, goodCases)
+    bad_key_cases = [('empty', empty)]
+    bad_value_cases = [('noneAddress', noneAddress), ('nonePort', nonePort), ('badPort', badPort),
+                       ('badFiles', badFiles)]
+    good_cases = [('random1', random1), ('random2', random2), ('authenticated', authenticated), ('complete', complete)]
+    test_bad_key_cases(task, bad_key_cases)
+    test_bad_value_cases(task, bad_value_cases)
+    test_good_cases(task, good_cases)
 
 if __name__ == '__main__':
-    testSamba()
+    test_samba()
