559d3d0f3cce878bfaa4f0f46debcd92836c7cdb
Joseph Vessella <jvessella@cert.org>
2017-08-02 15:28:21 -0400

    Added hook for Linux geckodriver.

--- hooks/hook-tasks.firefox.py
+++ hooks/hook-tasks.firefox.py
@@ -1,4 +1,4 @@
 ### Hooks script for PyInstaller to correctly package all modules.
 ### PyInstaller must be run with the --additional-hooks-dir option.
 
-datas = [('geckodriver/geckodriver.exe', 'geckodriver')]
+datas = [('geckodriver/geckodriver' + ext, 'geckodriver') for ext in (str(), '.exe')]
