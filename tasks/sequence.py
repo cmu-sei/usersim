5fe5567ad9429624c373ab2a914f0dc61d1ed716
Joseph Vessella <jvessella@cert.org>
2017-09-20 15:02:44 -0400

    [GUS-2509] Sequence task and test added.

---
+++ tasks/sequence.py
@@ -0,0 +1,56 @@
+import api
+from tasks import task
+
+
+class Sequence(task.Task):
+    """ Executes nested tasks in sequence.
+    """
+    def __init__(self, config):
+        self._tasks = config['tasks']
+        self._waiting = False
+        self._index = 0
+
+    def __call__(self):
+        if not self._waiting and self._index < len(self._tasks):
+            # Start the next task and wait for it.
+            self._current = api.new_task(self._tasks[self._index])
+            self._waiting = True
+            self._index += 1
+        if api.status_task(self._current)['state'] == api.States.STOPPED:
+            self._waiting = False
+
+    def stop(self):
+        if not self._waiting and self._index >= len(self._tasks):
+            # Wait until the last task has been stopped before letting this one stop.
+            return True
+        else:
+            return False
+
+    def status(self):
+        # Make sure it doesn't change in the middle of creating the string.
+        index = self._index
+
+        # Using index as-is here is a shortcut because a user is most likely most familiar with 1-based counting,
+        # but still want to give back the correct task type.
+        return 'Most recently triggered task {} which has a task type of {}.'.format(index,
+                self._tasks[index - 1]['type'])
+
+    def cleanup(self):
+        pass
+
+    @classmethod
+    def parameters(cls):
+        required = {'tasks': '[task]: A list of tasks. Must contain at least two tasks.'}
+        optional = dict()
+
+        return {'required': required, 'optional': optional}
+
+    @classmethod
+    def validate(cls, config):
+        config = api.check_config(config, cls.parameters(), dict())
+
+        # It doesn't make sense to use this task if there are fewer than two tasks.
+        if len(config['tasks']) < 2:
+            raise ValueError('{}: {} Must contain at least two tasks.'.format('tasks', config['tasks']))
+
+        return config
---
+++ tests/sequence.py
@@ -0,0 +1,41 @@
+import api
+import usersim
+
+
+def run_test():
+    test_1 = {'type': 'test',
+              'config': {}}
+    test_2 = {'type': 'testfeedback',
+              'config': {}}
+    config = {'type': 'sequence',
+              'config': {'tasks': [test_1, test_2]}}
+
+    sim = usersim.UserSim(True)
+
+    task_id = api.new_task(config)
+
+    print('Cycle 1')
+    sim.cycle()
+
+    print(api.status_task(task_id))
+
+    print('Cycle 2')
+    sim.cycle()
+
+    print(api.status_task(task_id))
+
+    print('Cycle 3')
+    sim.cycle()
+
+    # The sequence task may be stopped here, if it goes after the testfeedback task.
+    print(api.status_task(task_id))
+
+    print('Cycle 4')
+    sim.cycle()
+
+    # Otherwise it would be stopped here.
+    print(api.status_task(task_id))
+
+
+if __name__ == '__main__':
+    run_test()
